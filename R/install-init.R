61ab22553c30ada26fc3048fff6f7cdfc13db04a
Spencer Woody <spencer.woody@utexas.edu>
2018-05-10 02:04:14 -0500

    script for installing packages for new install of R

---
+++ R/install-init.R
@@ -0,0 +1,17 @@
+
+install.packages(
+  c(
+    "tidyverse",
+    "Rcpp",
+    "RcppArmadillo",
+    "RcppEigen",
+    "RcppParallel",
+    "RcppProgress",
+    "glmnet",
+    "caret",
+    "pROC"
+    "ggthemes",
+    "latex2exp",
+    "mvtnorm"
+  )
+)

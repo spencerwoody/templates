872cca3e9cd7907737072f47d1e91370f443d007
Spencer Woody <spencer.woody@utexas.edu>
2018-12-05 15:59:53 -0600

    add dependencies for R

--- beowulf-setup.sh
+++ beowulf-setup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-apt install git vim emacs25 r-base r-base-dev
+apt install git vim emacs25 r-base r-base-dev texlive-full dirmngr nfs-kernel-server evince nfs-common mpich libxml2-dev libcurl4-openssl-dev libssl-dev ufw
 
 echo "deb http://cran.revolutionanalytics.com/bin/linux/debian stretch-cran35/" | sudo tee -a /etc/apt/sources.list
 
 apt-key adv --keyserver keys.gnupg.net --recv-key 'E19F5F87128899B192B1A2C2AD5F960A256A04AF'
 
-add-apt-repository 'deb https://cloud.r-project.org/bin/linux/debian stretch-cran35/'
+apt update
+apt upgrade
 

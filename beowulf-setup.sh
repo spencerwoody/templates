c37ba04bbe5278c92dc3f9627123d1332a1edf43
Spencer Woody <spencer.woody@utexas.edu>
2018-12-05 15:44:21 -0600

    remove https, replace with http

--- beowulf-setup.sh
+++ beowulf-setup.sh
@@ -2,7 +2,7 @@
 
 apt install git vim emacs25 r-base r-base-dev
 
-echo "deb https://cran.revolutionanalytics.com/bin/linux/debian stretch-cran35/" | sudo tee -a /etc/apt/sources.list
+echo "deb http://cran.revolutionanalytics.com/bin/linux/debian stretch-cran35/" | sudo tee -a /etc/apt/sources.list
 
 apt-key adv --keyserver keys.gnupg.net --recv-key 'E19F5F87128899B192B1A2C2AD5F960A256A04AF'
 

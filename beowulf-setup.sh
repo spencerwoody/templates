0add7a954ff193d47e4dae8abc790209a6e5b51c
Spencer Woody <spencer.woody@utexas.edu>
2018-12-05 15:36:17 -0600

    update cran mirror

--- beowulf-setup.sh
+++ beowulf-setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-apt install git vim emacs r-base
+apt install git vim emacs r-base r-base-dev
+
+echo "deb https://cran.revolutionanalytics.com/bin/linux/debian stretch-cran35/" | sudo tee -a /etc/apt/sources.list
 
 apt-key adv --keyserver keys.gnupg.net --recv-key 'E19F5F87128899B192B1A2C2AD5F960A256A04AF'
